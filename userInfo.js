e39e4ddf230d3d19ead1d176a90f7fc040e9d3dc
caroldanso <cnaadanso@st.ug.edu.gh>
2024-05-18 21:08:42 +0000

    task 3

---
+++ userInfo.js
@@ -0,0 +1,13 @@
+// createUserProfiles
+
+function createUserProfiles(names, modifiedNames) {
+    return names.map((name, index) => {
+        return {
+            originalName: name,
+            modifiedName: modifiedNames[index],
+            id: index + 1
+        };
+    });
+}
+
+module.exports = { createUserProfiles };
